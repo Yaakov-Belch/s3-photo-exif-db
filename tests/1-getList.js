6354a734c9985db41d5076c2e0d035275a655ba6
Yaakov Belch <github@yaakovnet.net>
2016-11-11 02:13:22 +0200

    ok

--- tests/1-getList.js
+++ tests/1-getList.js
@@ -1,17 +1,22 @@
 const test = require('blue-tape');
-const getPhotoList=require('../lib/getPhotoList.js');
+const s3=require('../lib/s3access.js');
 
-const bucketUrl='http://s3.amazonaws.com/waldo-recruiting';
-const firstId='hello';
-const sampleLength=3;
+const bucketSpec={
+  endPoint: 's3.amazonaws.com',
+  bucket: 'waldo-recruiting'
+};
+
+const sampleLength=129;
+const firstId=
+  '0003b8d6-d2d8-4436-a398-eab8d696f0f9.68cccdd4-e431-457d-8812-99ab561bf867.jpg';
 
 test('getPhotoList sample from bucketUrl', t=>{
   t.plan(3);
-  getPhotoList(bucketUrl)
+  s3.getPhotoIdList(bucketSpec)
     .then(list=>{
       t.ok(Array.isArray(list),'return an array of ids');
-      t.equal(list[0],firstId,'first photo id');
       t.equal(list.length,sampleLength, 'length of sample list');
+      t.equal(list[0],firstId,'first photo id');
     })
     .catch(e=>t.fail(e));
 });
